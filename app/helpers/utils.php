eeb7feb60f84b060dfa42d78ab20e5aa820a50cf
nicmackenzie <nicmackenzie@live.com>
2024-06-04 22:52:18 +0300

    refactor: functions to get user menu

--- app/helpers/utils.php
+++ app/helpers/utils.php
@@ -15,3 +15,101 @@ function singleset($con,$sql,$arr){
     $stmt->execute($arr);
     return $stmt->fetch(PDO::FETCH_OBJ);
 }
+
+function getdbvalue($con,$sql,$arr){
+    $stmt = $con->prepare($sql);
+    $stmt->execute($arr);
+    return $stmt->fetchColumn();
+}
+
+function getusermenuitems($con,$user_id)
+{
+    $role = getdbvalue($con,'SELECT role_id FROM users WHERE id=?',[$user_id]);
+    $is_admin = (int)$role < 3; 
+    if ($is_admin)
+    {
+        $sql = 'SELECT 
+                    DISTINCT module
+                FROM
+                    forms
+                ORDER BY
+                    module_id
+        ';
+        $stmt = $con->prepare($sql);
+        $stmt->execute();
+    }
+    else
+    {
+        $has_rights = getdbvalue($con,'SELECT COUNT(*) FROM user_rights WHERE user_id=?',[$user_id]) > 0;
+        if($has_rights){
+            $sql = 'SELECT 
+                        DISTINCT f.module 
+                    FROM 
+                        user_rights r INNER JOIN forms f on r.form_id = f.id 
+                    WHERE (r.user_id = ?)
+                    ORDER BY f.module_id';
+            $stmt = $con->prepare($sql);
+            $stmt->execute([$user_id]);
+        }
+        if(!$has_rights && !is_null($role)){
+            $sql = 'SELECT 
+                        DISTINCT f.module 
+                    FROM 
+                        role_rights r INNER JOIN forms f on r.form_id = f.id 
+                    WHERE (r.role_id = ?)
+                    ORDER BY f.module_id';
+            $stmt = $con->prepare($sql);
+            $stmt->execute([$role]);
+        }
+        if(!$has_rights && !$is_admin){
+            return [];
+        }    
+    }    
+    $results = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $modules = array();
+    foreach($results as $result) {
+        array_push($modules,$result->module);
+    }
+    return $modules;
+}
+
+function getmodulemenuitems($con,$userid,$module)
+{
+    $role = getdbvalue($con,'SELECT role_id FROM users WHERE id=?',[$userid]);
+    $is_admin = (int)$role < 3; 
+
+    if($is_admin){
+        $sql = 'SELECT form_name,
+                       path
+                FROM   forms
+                WHERE  (module = :menu)
+                ORDER BY menu_order';
+        $stmt = $con->prepare($sql);
+    }else{
+        $has_rights = getdbvalue($con,'SELECT COUNT(*) FROM user_rights WHERE user_id=?',[$userid]) > 0;
+        if($has_rights){
+            $sql = 'SELECT f.form_name,
+                        f.path
+                    FROM   user_rights r inner join forms f on r.form_id = f.id
+                    WHERE  r.user_id = :usid AND (f.module = :menu)
+                    ORDER BY menu_order';
+            $stmt = $con->prepare($sql);
+            $stmt->bindValue(':usid',$userid);
+        }
+        if(!$has_rights && !is_null($role)){
+            $sql = 'SELECT f.form_name,
+                        f.path
+                    FROM   role_rights r inner join forms f on r.form_id = f.id
+                    WHERE  r.role_id = :roleid AND (f.module = :menu)
+                    ORDER BY menu_order';
+            $stmt = $con->prepare($sql);
+            $stmt->bindValue(':roleid',$role);
+        }
+        if(!$has_rights && is_null($role)){
+            return [];
+        }         
+    }
+    $stmt->bindValue(':menu',$module);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_OBJ);
+}
