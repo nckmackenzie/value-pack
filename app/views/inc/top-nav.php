c5add9fa050252b370b321482539d3d3b12d2f96
nicmackenzie <nicmackenzie@live.com>
2024-06-04 22:51:54 +0300

    feat: set layout:header

---
+++ app/views/inc/top-nav.php
@@ -0,0 +1,34 @@
+<div class="wrapper">
+    <nav class="main-header navbar navbar-expand navbar-dark navbar-navy text-sm">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"
+              ><i class="fas fa-bars"></i
+            ></a>
+          </li>
+        </ul>
+        <div class="relative ml-auto">
+          <img src="https://github.com/shadcn.png" alt="Avatar" class="w-8 h-8 rounded-full cursor-pointer" id="avatar">
+          <div id="dropdown" class="hidden absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-20 py-2">
+            <div class="text-center text-gray-400 text-xs uppercase font-medium">Administrator</div>
+            <ul class="py-1 pt-3">
+              <li class="px-4 py-2 text-sm text-gray-700 cursor-pointer hover:bg-gray-100 group">
+                <a href="" class="flex items-center gap-2 transition-colors">
+                  <i data-lucide="lock" class="size-4 text-slate-400 transition-colors group-hover:text-blue-400"></i>
+                  <span class="transition-colors group-hover:text-blue-400">Change Password</span>
+                </a>
+              </li>
+              <li class="px-4 py-2 text-sm text-gray-700 cursor-pointer hover:bg-gray-100 group">
+                <a href="" class="flex items-center gap-2">
+                  <i data-lucide="log-out" class="size-4 text-slate-400 transition-colors group-hover:text-blue-400"></i>
+                  <span class="transition-colors group-hover:text-blue-400">Logout</span>
+                </a>
+              </li>
+              <!-- <li class="px-4 py-2 text-sm text-gray-700 cursor-pointer hover:bg-gray-100">Logout</li> -->
+            </ul>
+          </div>
+        </div>
+        <!-- <ul class="navbar-nav ml-auto">
+            <a href="<?php echo URLROOT;?>/users/logout" class="btn btn-flat text-white">Logout</a>
+        </ul> -->
+    </nav>
