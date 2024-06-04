a44c28adc1d03b215095343c7c92f3ed0b21f281
nicmackenzie <nicmackenzie@live.com>
2024-06-04 22:19:34 +0300

    chore: layout definition:footer

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,34 @@
-<script src="<?php echo URLROOT; ?>/js/main.js"></script>
-</body>
-</html>
+    <footer class="main-footer">
+        <div class="float-right d-none d-sm-inline">v1.0.1</div>
+        <strong>Copyright &copy; <?php echo date("Y");?>
+            <a href="#">Designed By Mack Softwares</a>.
+        </strong>
+        All rights reserved.
+    </footer>
+</div>
+<script src="<?php echo URLROOT; ?>/plugins/jquery/jquery.min.js"></script>
+<script src="<?php echo URLROOT; ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/jquery-validation/jquery.validate.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/jquery-validation/additional-methods.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/jszip/jszip.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/select2/dist/js/select2.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/moment/moment.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/CustomDate/dist/DateTimePicker.min.js"></script>
+<script src="<?php echo URLROOT;?>/plugins/bs-multiselect/dist/js/bootstrap-select.min.js"></script>
+<script src="<?php echo URLROOT; ?>/js/adminlte.min.js"></script>
+<script src="https://unpkg.com/lucide@latest"></script>
+  <script>
+    lucide.createIcons();
+  </script>
