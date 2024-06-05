923b46f7fede68e6fd5818ab308180692eb46f04
nicmackenzie <nicmackenzie@live.com>
2024-06-05 19:14:39 +0300

    chore: template for a blank setup

---
+++ app/views/inc/blank.php
@@ -0,0 +1,26 @@
+<?php require APPROOT . '/views/inc/header.php';?>
+<?php require APPROOT . '/views/inc/top-nav.php';?>
+<?php require APPROOT . '/views/inc/side-nav.php';?>
+  <div class="content-wrapper">
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Blank Page</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Blank Page</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="content">
+
+    </section><!-- /.content -->
+</div><!-- /.content-wrapper -->
+<?php require APPROOT . '/views/inc/footer.php'?>
+</body>
+</html>  
