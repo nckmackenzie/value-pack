6f8847bb5ee57ec1377c20da91e4760648f712da
nicmackenzie <nicmackenzie@live.com>
2024-06-06 21:35:01 +0300

    refacor: change field names

--- app/models/Reusable.php
+++ app/models/Reusable.php
@@ -10,7 +10,7 @@ class Reusable
     
     public function get_stores()
     {
-        $sql = 'SELECT ID,UPPER(Store_Name) as StoreName FROM stores WHERE Active=?';
+        $sql = 'SELECT ID,UPPER(Store_Name) as Store_Name FROM stores WHERE Active=?';
         return resultset($this->db->dbh,$sql,[true]);
     }
 }
