19be7c52eac6fe91fd5b3374354709dd1a9727b0
nicmackenzie <nicmackenzie@live.com>
2024-06-06 20:38:06 +0300

    feat: layout for side nav

---
+++ app/views/inc/side-nav.php
@@ -0,0 +1,45 @@
+<?php
+    $con = new Database;
+    // $menuitems = getusermenuitems($con->dbh,(int)$_SESSION['userId']);
+    $menuitems = getusermenuitems($con->dbh,'6503a5k0');
+    $menuicons = [
+        'admin' => 'shield-check',
+        'transactions' => 'sliders-horizontal',
+        'reports' => 'file-text',
+    ];
+?>
+<aside class="main-sidebar bg-slate-50 text-slate-700 shadow">
+    <a href="<?php echo URLROOT;?>/dashboard" class="flex justify-center items-center h-12">
+        <span class="text-blue-600 font-bold">VALUE PACK</span>
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar text-sm ">
+        
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
+                <?php foreach($menuitems as $menuitem) :?>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link flex items-center transition-all hover:bg-gray-200/50">
+                            <i data-lucide="<?php echo $menuicons[$menuitem];?>" class="size-4 text-gray-400 mr-2"></i>
+                            <p class="capitalize text-sm font-semibold">
+                                <?php echo $menuitem ;?>
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview custom-font">
+                            <!-- $navitems = getmodulemenuitems($con->dbh,(int)$_SESSION['userId'],$menuitem)  -->
+                            <?php $navitems = getmodulemenuitems($con->dbh,'6503a5k0',$menuitem) ;?>
+                            <?php foreach($navitems as $navitem) : ?>
+                                <li class="nav-item transition-all hover:bg-gray-200/50">
+                                    <a href="<?php echo URLROOT;?>/<?php echo $navitem->path;?>" class="nav-link">
+                                        <p class="text-xs"><?php echo ucwords($navitem->form_name);?></p>
+                                    </a>
+                                </li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </li>
+                <?php endforeach;?>
+            </ul>
+        </nav>
+    </div><!-- /.sidebar -->
+</aside>   
