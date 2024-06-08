7da0169e3d972e90b464377309adc2c7bec27860
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-08 20:21:49 +0300

    feat: datatable resuable functions

--- public/js/utils/datatable.js
+++ public/js/utils/datatable.js
@@ -1,9 +1,20 @@
+const deletes = document.querySelectorAll('.btndel');
 export function basicDatatable(table, columnDefs, ordering = false) {
   $(document).ready(function () {
     'use strict';
     $(`#${table}`).DataTable({
       ordering: ordering,
       columnDefs,
+    });
+  });
+}
+
+export function deleteButtonClick(datatableId, modalId, inputId) {
+  $(document).ready(function () {
+    $(`#${datatableId}`).on('click', '.btndel', function () {
+      $(`#${modalId}`).modal('show');
+
+      $(`#${inputId}`).val(this.dataset.id);
     });
   });
 }
