12226be80d081f7ea1551f02777e6cff19e518d1
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-08 19:57:33 +0300

    feat: components for actions and delete modal

---
+++ app/helpers/components.php
@@ -0,0 +1,45 @@
+<?php
+function action_buttons( $action = "", $path = "", $id = 0) {
+    if($action === 'edit'){
+        echo "<a href=".URLROOT."/$path/edit/$id class='flex items-center gap-1 group'>
+                <i data-lucide='square-pen' class='size-3 text-emerald-300 group-hover:text-emerald-200'></i>
+                <span class='text-emerald-400 transition-colors font-medium text-xs group-hover:text-emerald-300'>Edit</span>
+              </a>";
+    }else if($action === "delete"){
+        echo "<button type='button' class='flex items-center gap-1 group btndel outline-none focus:outline-none focus:border-none' data-id='$id'>
+                <i data-lucide='trash' class='size-3 text-rose-300 group-hover:text-rose-200'></i>
+                <span class='text-rose-400 transition-colors font-medium text-xs group-hover:text-rose-300'>Delete</span>
+              </button>";
+    }
+}
+
+function DeleteModal($route,$modalid,$message,$inputid){
+    echo "
+       <div class='modal fade' id='$modalid' tabindex='-1' role='dialog' aria-labelledby='exampleModalCenterTitle' aria-hidden='true'>
+            <div class='modal-dialog modal-dialog-centered' role='document'>
+                <div class='modal-content'>
+                    <div class='modal-header'>
+                        <h5 class='modal-title' id='exampleModalLongTitle'>Delete Transaction</h5>
+                        <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+                            <span aria-hidden='true'>&times;</span>
+                        </button>
+                    </div>
+                    <div class='modal-body'>
+                        <form action='$route' method='post'>
+                            <div class='row'>
+                                <div class='col-12'>
+                                    <label for='$inputid'>$message</label>
+                                    <input type='hidden' name='id' id='$inputid'>
+                                </div>
+                            </div>
+                            <div class='modal-footer'>
+                                <button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>
+                                <button type='submit' class='btn btn-danger'>Yes</button>
+                            </div>
+                        </form>
+                    </div>       
+                </div>
+            </div>
+        </div>
+    ";
+}
