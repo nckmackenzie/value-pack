b0e890b36c0d6dfbf1ad89cb4651b38cc5bcfe2f
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-08 20:26:15 +0300

    feat: functionalities for update and delete of store

--- app/controllers/Stores.php
+++ app/controllers/Stores.php
@@ -25,6 +25,7 @@ class Stores extends Controller
             'id' => '',
             'is_edit' => false,
             'store_name' => '',
+            'active' => true,
             'store_name_err' => '',
             'error' => null
         ];
@@ -40,7 +41,8 @@ class Stores extends Controller
         }
 
         $is_edit = filter_input(INPUT_POST, 'is_edit', FILTER_VALIDATE_BOOLEAN);
-        $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT); 
+        $active = filter_input(INPUT_POST, 'active', FILTER_VALIDATE_BOOLEAN);
+        $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_SPECIAL_CHARS); 
         $store_name = filter_input(INPUT_POST, 'store_name', FILTER_SANITIZE_SPECIAL_CHARS); 
 
         $data = [
@@ -48,6 +50,7 @@ class Stores extends Controller
             'id' => !empty($id) ? $id : '',
             'is_edit' => $is_edit,
             'store_name' => !empty($store_name) ? trim($store_name) : null,
+            'active' => $is_edit ? $active : true,
             'store_name_err' => '',
             'error' => null
         ];
@@ -74,5 +77,51 @@ class Stores extends Controller
         }
 
         redirect('stores');
+    }
+
+    public function edit($id)
+    {
+        $store = $this->storemodel->get_store($id);
+        $data = [
+            'title' => 'Edit store details',
+            'id' => $store->ID,
+            'is_edit' => true,
+            'store_name' => strtoupper($store->Store_Name),
+            'active' => (bool)$store->Active,
+            'store_name_err' => '',
+            'error' => null
+        ];
+        $this->view('stores/new',$data);
+    }
+
+    public function delete()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+            flash('store_msg','Invalid request method', alert_type('error'));
+            redirect('stores/new');
+            exit();
+        }
+
+        $id = filter_input(INPUT_POST,'id',FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if(empty($id) || is_null($id)){
+            flash('store_msg','Unable to get selected id.', alert_type('error'));
+            redirect('stores');
+            exit();
+        }
+
+        if($this->storemodel->is_referenced($id)){
+            flash('store_msg','Unable to delete store as its referenced elsewhere.', alert_type('error'));
+            redirect('stores');
+            exit();
+        }
+
+        if(!$this->storemodel->delete($id)){
+            flash('store_msg','Something went wrong while deleting store.', alert_type('error'));
+            redirect('stores');
+            exit();
+        }
+        flash('store_msg','Store deleted successfully', alert_type('success'));
+        redirect('stores'); 
     }
 }
--- app/models/Store.php
+++ app/models/Store.php
@@ -17,9 +17,16 @@ class Store
         return $count > 0;
     }
 
+    public function get_store($id){
+        return singleset($this->db->dbh,'SELECT * FROM stores WHERE ID=?',[$id]);
+    }
+
     public function create_update($data){
         if($data['is_edit']){
-
+            $this->db->query('UPDATE stores SET Store_Name=:store, Active=:active WHERE ID = :id');
+            $this->db->bind(':store',strtolower($data['store_name']));
+            $this->db->bind(':active',$data['active']);
+            $this->db->bind(':id',$data['id']);
         }else{
             $this->db->query('INSERT INTO stores (ID,Store_Name) VALUES (:id,:store)');
             $this->db->bind(':id',cuid());
@@ -29,5 +36,26 @@ class Store
             return false;
         }
         return true;
+    }
+
+    public function is_referenced($id):bool
+    {
+        
+        $count =  (int)getdbvalue($this->db->dbh,'SELECT COUNT(*) FROM user_stores WHERE store_id = ?',[$id]);
+        if($count > 0){
+            return true;
+        }
+        return false;
+    }
+
+    public function delete($id)
+    {
+        $this->db->query('DELETE FROM stores WHERE ID = :id');
+        $this->db->bind(':id', $id);
+        if(!$this->db->execute()){
+            return false;
+        }else{
+            return true;
+        }
     }
 }
