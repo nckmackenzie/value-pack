1227a0b20cb35dafb480c0bb103180f3a7ea2ace
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-09 11:56:44 +0300

    chore: method to return units

--- app/models/Reusable.php
+++ app/models/Reusable.php
@@ -13,4 +13,10 @@ class Reusable
         $sql = 'SELECT ID,UPPER(Store_Name) as Store_Name FROM stores WHERE Active=?';
         return resultset($this->db->dbh,$sql,[true]);
     }
+
+    public function get_units()
+    {
+        $sql = 'SELECT id,UPPER(unit) as unit FROM units ORDER BY unit';
+        return resultset($this->db->dbh,$sql,[]);
+    }
 }
