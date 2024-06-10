f47686f5eb0b0e8384eb20ed0271e456865f75df
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-10 15:36:18 +0300

    refactor: change datatable styles

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -10,19 +10,15 @@
 <script src="<?php echo URLROOT; ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo URLROOT;?>/plugins/jquery-validation/jquery.validate.min.js"></script>
 <script src="<?php echo URLROOT;?>/plugins/jquery-validation/additional-methods.min.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/jszip/jszip.min.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/pdfmake/pdfmake.min.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/pdfmake/vfs_fonts.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="<?php echo URLROOT;?>/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- DATATABLES -->
+
+<script src="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.0.8/b-3.0.2/b-colvis-3.0.2/b-html5-3.0.2/b-print-3.0.2/r-3.0.2/sl-2.0.3/datatables.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+<!-- DATATABLES -->
 <script src="<?php echo URLROOT;?>/plugins/select2/dist/js/select2.min.js"></script>
+
+
 <script src="<?php echo URLROOT;?>/plugins/moment/moment.min.js"></script>
 <script src="<?php echo URLROOT;?>/plugins/daterangepicker/daterangepicker.js"></script>
 <script src="<?php echo URLROOT;?>/plugins/CustomDate/dist/DateTimePicker.min.js"></script>
