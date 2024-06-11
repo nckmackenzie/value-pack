a27571335ef8f79faf25829322b49f3d83ebb0ed
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-11 16:53:38 +0300

    feat: vat management

--- public/js/pages/purchases/purchase.js
+++ public/js/pages/purchases/purchase.js
@@ -1,14 +1,46 @@
 import { basicDatatable } from '../../utils/datatable.js';
+import { numberWithCommas } from '../../utils/formatters.js';
+import { getSelectedText } from '../../utils/helpers.js';
 
 const table = document.getElementById('items_table');
 const tbody = table?.getElementsByTagName('tbody')[0];
 const addButton = document.getElementById('add');
-const productInput = document.getElementById('product');
+const productSelect = document.getElementById('product');
+const vatTypeSelect = document.getElementById('vat_type');
+const vatSelect = document.getElementById('vat');
 const qtyInput = document.getElementById('qty');
 const rateInput = document.getElementById('rate');
 const valueInput = document.getElementById('value');
+const totalInput = document.getElementById('total');
 
 const items = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+  function createItemObject(row) {
+    const inputs = row.querySelectorAll('input');
+    return {
+      productId: inputs[0].value,
+      productName: inputs[1].value,
+      qty: parseInt(inputs[2].value, 10),
+      rate: parseFloat(inputs[3].value.replace(',', '')),
+      value: parseFloat(inputs[4].value.replace(',', '')),
+    };
+  }
+
+  tbody?.querySelectorAll('tr').forEach(row => {
+    const item = createItemObject(row);
+    items.push(item);
+  });
+});
+
+vatTypeSelect?.addEventListener('change', e => {
+  if (e.target.value === 'no-vat') {
+    vatSelect.disabled = true;
+  } else {
+    vatSelect.disabled = false;
+  }
+  vatSelect.value = '';
+});
 
 function updateTable() {
   tbody.innerHTML = '';
@@ -16,27 +48,87 @@ function updateTable() {
     .map(
       item =>
         ` <tr>
-            <td>${item.productName}</td>
-            <td>${item.qty}</td>
-            <td>${item.rate}</td>
-            <td>${item.value}</td>
-            <td><button type="button">Remove</button></td>
+            <td class="hidden"><input type="text" name="product_id[]" value="${item.productId}"/></td>
+            <td><input type="text" name="product[]" class="w-full" value="${item.productName}"/></td>
+            <td><input type="number" name="qty[]" value="${item.qty}" /></td>
+            <td><input type="number" name="rate[]" value="${item.rate}" /></td>
+            <td><input type="number" name="value[]" value="${item.value}" /></td>
+            <td><button type="button" class="outline-none border-none text-rose-400 focus:outline-0">Remove</button></td>
         </tr>
     `
     )
     .join('');
-  console.log(itemsMarkup);
+  tbody.innerHTML = itemsMarkup;
+
+  const total = items
+    .reduce((acc, item) => acc + item.value, 0)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  totalInput.value = total;
 }
 
+function getTotal(qty, rate) {
+  return numberWithCommas(qty * rate);
+}
+
+rateInput?.addEventListener('blur', e => {
+  if (
+    !e.target.value ||
+    e.target.value.toString().trim().length === 0 ||
+    !qtyInput.value ||
+    qtyInput.value.toString().trim().length === 0
+  ) {
+    return;
+  }
+
+  valueInput.value = getTotal(+qtyInput.value, +e.target.value);
+});
+
+qtyInput?.addEventListener('blur', e => {
+  if (
+    !e.target.value ||
+    e.target.value.toString().trim().length === 0 ||
+    !rateInput.value ||
+    rateInput.value.toString().trim().length === 0
+  ) {
+    return;
+  }
+
+  valueInput.value = getTotal(+rateInput.value, +e.target.value);
+});
+
 addButton?.addEventListener('click', () => {
+  if (!productSelect.value || !qtyInput.value || !rateInput.value) {
+    alert('Please fill all the fields');
+    return;
+  }
+
+  const itemIsSet = items.some(item => item.productId === productSelect.value);
+  if (itemIsSet) {
+    alert('This item has already been entered.');
+    return;
+  }
+
   const newItem = {
-    productName: productInput.value,
+    productId: productSelect.value,
+    productName: getSelectedText(productSelect),
     qty: qtyInput.value,
     rate: rateInput.value,
     value: +qtyInput.value * +rateInput.value,
   };
   items.push(newItem);
   updateTable();
+  qtyInput.value =
+    rateInput.value =
+    valueInput.value =
+    productSelect.value =
+      '';
+});
+
+table?.addEventListener('click', e => {
+  if (e.target.classList.contains('text-rose-400')) {
+    e.target.closest('tr').remove();
+  }
 });
 
 basicDatatable('purchasesDatatable', [{ width: '10%', targets: 4 }]);
