c18b64597b65b6c18eecb32990df0d7e81beda56
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-11 16:53:13 +0300

    feat: create purchases

--- app/controllers/Purchases.php
+++ app/controllers/Purchases.php
@@ -37,12 +37,106 @@ class Purchases extends Controller
             'vat_type' => '',
             'vat' => '',
             'store' => '',
+            'items' => [],
+            'total' => 0,
             'store_err' => '',
             'vendor_err' => '',
             'date_err' => '',
             'vat_type_err' => '',
             'vat_err' => '',
+            'error' => null
         ];
         $this->view('purchases/new', $data);
+    }
+
+    public function create_update()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+            flash('purchase_msg','Invalid request method.');
+            redirect('purchases');
+            exit();
+        }
+
+        $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_SPECIAL_CHARS);
+        $vendor = filter_input(INPUT_POST, 'vendor', FILTER_SANITIZE_SPECIAL_CHARS);
+        $reference = filter_input(INPUT_POST, 'reference', FILTER_SANITIZE_SPECIAL_CHARS);
+        $vat_type = filter_input(INPUT_POST, 'vat_type', FILTER_SANITIZE_SPECIAL_CHARS);
+        $vat = filter_input(INPUT_POST, 'vat', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $store = filter_input(INPUT_POST, 'store', FILTER_SANITIZE_SPECIAL_CHARS);
+        $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
+        $is_edit = filter_input(INPUT_POST,'is_edit',FILTER_VALIDATE_BOOLEAN);
+        $products = isset($_POST['product']) ? $_POST['product'] : [];
+        $product_ids = isset($_POST['product_id']) ? $_POST['product_id'] : [];
+        $quantities = isset($_POST['qty']) ? $_POST['qty'] : [];
+        $rates = isset($_POST['rate']) ? $_POST['rate'] : [];
+        
+        $data = [
+            'title' => $is_edit ? 'Update purchase' : 'Create purchase',
+            'products' => $this->reusablemodel->get_products(),
+            'suppliers' => $this->reusablemodel->get_vendors(),
+            'stores' => $this->reusablemodel->get_stores(),
+            'id' => $is_edit ? $id : cuid(),
+            'is_edit' => $is_edit,
+            'date' => !empty($date) ? date('Y-m-d',strtotime($date)) : null ,
+            'vendor' => !empty($vendor) ? trim($vendor) : null,
+            'reference' => !empty($reference) ? trim($reference) : null,
+            'vat_type' => !empty($vat_type) ? trim($vat_type) : null,
+            'vat' => !empty($vat) ? floatval($vat) : null,
+            'store' => !empty($store) ? trim($store) : null, 
+            'items' => [],
+            'total' => 0,
+            'store_err' => '',
+            'vendor_err' => '',
+            'date_err' => '',
+            'vat_type_err' => '',
+            'vat_err' => '',
+            'error' => null
+        ];
+
+        if(is_null($data['date'])){
+            $data['date_err'] = 'Select date';
+        }
+        if(is_null($data['vendor'])){
+            $data['vendor_err'] = 'Select vendor';
+        }
+        if(is_null($data['vat_type'])){
+            $data['vat_type_err'] = 'Select vat type';
+        }
+        if(is_null($data['store'])){
+            $data['store_err'] = 'Select store';
+        }
+        if(!is_null($data['vat_type']) && $data['vat_type'] !== 'no-vat' && is_null($data['vat'])){
+            $data['vat_err'] = 'Select vat';
+        }
+
+        if(count($products) === 0){
+            $data['error'] = 'No products added';
+        }
+
+        if(!is_null($data['error']) || !empty($data['date_err']) || !empty($data['store_err']) || !empty($data['vat_type_err'])
+            || !empty($data['vat_err']) || !empty($data['store_err'])){
+            $this->view('purchases/new', $data);
+            exit();
+        }
+
+        for ($i=0; $i < count($products); $i++) { 
+            array_push($data['items'],[
+                'product_id' => $product_ids[$i],
+                'product_name' => $products[$i],
+                'qty' => $quantities[$i],
+                'rate' => $rates[$i],
+                'value' => floatval($quantities[$i]) * floatval($rates[$i])
+            ]);
+        }
+
+        $data['total'] = array_sum(array_column($data['items'], 'value'));
+
+        if(!$this->purchasemodel->create_update($data)){
+            $data['error'] = 'Something went wrong while performing this action.';
+            $this->view('purchases/new',$data);
+            exit();
+        }
+
+        redirect('purchases');
     }
 }
--- app/models/Purchase.php
+++ app/models/Purchase.php
@@ -1,8 +1,73 @@
 <?php
 class Purchase
 {
+    private $db;
     public function __construct()
     {
-        
+        $this->db = new Database;
+    }
+
+    function save($data)
+    {
+        try {
+
+            $this->db->dbh->beginTransaction();
+
+            $sql = "INSERT INTO purchases_headers (id, purchase_date, supplier_id, reference, vat_type, vat, store_id,amount_exclusive, vat_amount, amount_inclusive,created_by) ";
+            $sql .= "VALUES (:id, :date, :vendor, :reference, :vat_type, :vat, :store, :amount_exclusive, :vat_amount, :amount_inclusive, :creator) ";
+
+            $this->db->query($sql);
+            $this->db->bind(':id', $data['id']);
+            $this->db->bind(':date', $data['date']);
+            $this->db->bind(':vendor', $data['vendor']);
+            $this->db->bind(':reference', strtolower($data['reference']));
+            $this->db->bind(':vat_type', $data['vat_type']);
+            $this->db->bind(':vat', $data['vat']);
+            $this->db->bind(':store', $data['store']);
+            $this->db->bind(':amount_exclusive', calculate_vat($data['vat_type'],$data['total'])[0]);
+            $this->db->bind(':vat_amount', calculate_vat($data['vat_type'],$data['total'])[1]);
+            $this->db->bind(':amount_inclusive', calculate_vat($data['vat_type'],$data['total'])[2]);
+            $this->db->bind(':creator', $_SESSION['user_id']);
+            $this->db->execute();
+
+            foreach($data['items'] as $item){
+                $this->db->query('INSERT INTO purchases_details (header_id, product_id, qty, rate) VALUES (:id, :product_id, :qty, :rate)');
+                $this->db->bind(':id', $data['id']);
+                $this->db->bind(':product_id', $item['product_id']);
+                $this->db->bind(':qty', $item['qty']);
+                $this->db->bind(':rate', $item['rate']);
+                $this->db->execute();
+
+                $this->db->query('INSERT INTO stock_movements (transaction_date,transaction_type,store_id,product_id,qty,transaction_id,created_by) VALUES (:date,:type,:store,:product,:qty,:tid,:creator)');
+                $this->db->bind(':date', $data['date']);
+                $this->db->bind(':type', 'purchase');
+                $this->db->bind(':store', $data['store']);
+                $this->db->bind(':product', $item['product_id']);
+                $this->db->bind(':qty', $item['qty']);
+                $this->db->bind(':tid', $data['id']);
+                $this->db->bind(':creator', $_SESSION['user_id']);
+                $this->db->execute();
+            }
+
+            if(!$this->db->dbh->commit()){
+                return false;
+            }
+
+            return true;
+            
+        } catch (\Exception $e) {
+            if ($this->db->dbh->inTransaction()) {
+                $this->db->dbh->rollback();
+            }
+            error_log($e->getMessage());
+            return false;
+        }
+    }
+
+    public function create_update($data)
+    {
+        if(!$data['is_edit']){
+            return $this->save($data);
+        }
     }
 }
