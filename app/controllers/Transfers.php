79d97f0ae2fec4af068925cd2290c706efedd96c
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-12 22:14:50 +0300

    feat: fetch and edit a transfer

--- app/controllers/Transfers.php
+++ app/controllers/Transfers.php
@@ -15,7 +15,7 @@ class Transfers extends Controller
 
     public function index()
     {
-        $data =['title' => 'Transfers','transfers' => []];
+        $data =['title' => 'Transfers','transfers' => $this->transfermodel->get_transfers()];
         $this->view('transfers/index',$data);
     }
 
@@ -25,16 +25,126 @@ class Transfers extends Controller
             'title' => 'Add transfer',
             'stores' => $this->reusablemodel->get_stores(),
             'products' => $this->reusablemodel->get_products_by_store($_SESSION['store']),
+            'transfer_no' => $this->transfermodel->get_transfer_no(),
             'is_edit' => false,
             'id' => '',
             'items' => [],
             'transfer_date' => date('Y-m-d'),
-            'transfer_no' => 1,
             'store' => '',
             'store_err' => '',
             'transfer_date_err' => '',
-            'error' => null
+            'errors' => []
         ];
+        $this->view('transfers/new',$data);
+    }
+
+    public function create_update()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+            flash('transfer_msg','Invalid request method', alert_type('error'));
+            redirect('transfers');
+            exit();
+        }
+
+        $transfer_date = filter_input(INPUT_POST,'transfer_date',FILTER_SANITIZE_SPECIAL_CHARS);
+        $to_store = filter_input(INPUT_POST,'store',FILTER_SANITIZE_SPECIAL_CHARS);
+        $product_ids = isset($_POST['product_id']) ? $_POST['product_id'] : [];
+        $products = isset($_POST['product']) ? $_POST['product'] : [];
+        $quantities = isset($_POST['qty']) ? $_POST['qty'] : [];
+        $id = filter_input(INPUT_POST,'id',FILTER_SANITIZE_SPECIAL_CHARS);
+        $is_edit = filter_input(INPUT_POST,'is_edit',FILTER_VALIDATE_BOOLEAN);
+
+        $data = [
+            'title' => $is_edit ? 'Update transfer' : 'Add transfer',
+            'stores' => $this->reusablemodel->get_stores(),
+            'products' => $this->reusablemodel->get_products_by_store($_SESSION['store']),
+            'transfer_no' => $this->transfermodel->get_transfer_no(),
+            'is_edit' => $is_edit,
+            'id' => !$is_edit ? cuid() : $id,
+            'items' => [],
+            'transfer_date' => !empty($transfer_date) ? date('Y-m-d',strtotime($transfer_date)) : '',
+            'store' => !empty($to_store) ? trim($to_store) : '',
+            'store_err' => '',
+            'transfer_date_err' => '',
+            'errors' => []
+        ];
+
+        if(empty($data['transfer_date'])){
+            $data['transfer_date_err'] = 'Select transfer date';
+        }
+        if(empty($data['store'])){
+            $data['store_err'] = 'Select store to transfer to';
+        }
+        if(count($product_ids) === 0){
+            array_push($data['errors'],'No products entered for transfer');
+        }else{
+            for ($i=0; $i < count($products); $i++) { 
+                array_push($data['items'],[
+                    'product_id' => $product_ids[$i],
+                    'product_name' => $products[$i],
+                    'qty' => $quantities[$i],
+                ]);
+            }
+        }
+
+        if(!empty($data['transfer_date_err']) || !empty($data['store_err']) || count($data['errors']) > 0){
+            $this->view('transfers/new',$data);
+            exit();
+        }
+
+        foreach($data['items'] as $item){
+            $curr_value = $this->reusablemodel->get_current_stock_balance($_SESSION['store'],$item['product_id'],$data['transfer_date']);
+            if((int)$curr_value < (int)$item['qty']){
+                array_push($data['errors'],'Insufficient stock for product '.$item['product_name']);
+            }
+        }
+
+        if(count($data['errors']) > 0){
+            $this->view('transfers/new',$data);
+            exit();
+        }
+
+        if(!$this->transfermodel->create_update($data)){
+            array_push($data['errors'],'Failed to save transfer.');
+            $this->view('transfers/new',$data);
+            exit();
+        }
+
+        redirect('transfers');
+    }
+
+    public function edit($id)
+    {
+        $transfer = $this->transfermodel->get_transfer($id);
+        if(!$transfer){
+            $this->not_found('/products', 'The transfer you are trying to edit doesn\'t exist');
+            exit();
+        }
+        $data = [
+            'title' => 'Update transfer',
+            'stores' => $this->reusablemodel->get_stores(),
+            'products' => $this->reusablemodel->get_products_by_store($_SESSION['store']),
+            'transfer_no' => $transfer->transfer_no,
+            'is_edit' => true,
+            'id' => $transfer->id,
+            'items' => [],
+            'transfer_date' => date('Y-m-d',strtotime($transfer->transfer_date)),
+            'store' => $transfer->store_to,
+            'store_err' => '',
+            'transfer_date_err' => '',
+            'errors' => []
+        ];
+
+        $products = $this->transfermodel->get_transfer_items($transfer->id);
+
+        foreach($products as $product){
+            array_push($data['items'],[
+                'product_id' => $product->product_id,
+                'product_name' => strtoupper($product->product_name),
+                'qty' => $product->qty,
+            ]);
+        }
+
         $this->view('transfers/new',$data);
     }
 }
--- app/models/Transfer.php
+++ app/models/Transfer.php
@@ -65,7 +65,66 @@ class Transfer
                 $this->db->bind(':qty', $item['qty'] * -1);
                 $this->db->bind(':tid', $data['id']);
                 $this->db->bind(':creator', $_SESSION['user_id']);
-                $this->db->execute();            }
+                $this->db->execute();        
+            }
+
+            if(!$this->db->dbh->commit()){
+                return false;
+            }
+            
+            return true;
+
+        } catch (\Exception $e) {
+            if($this->db->dbh->inTransaction()){
+                $this->db->dbh->rollback();
+            }
+            error_log($e->getMessage());
+            return false;
+        }
+    }
+
+    function update($data)
+    {
+        try {
+
+            $this->db->dbh->beginTransaction();
+
+            $sql = "UPDATE transfers_headers SET transfer_date = :transfer_date, store_to = :store_to, created_by = :creator ";
+            $sql .= "WHERE (id = :id) ";
+
+            $this->db->query($sql);
+            $this->db->bind(':transfer_date', $data['transfer_date']);
+            $this->db->bind(':store_to', $data['store']);
+            $this->db->bind(':creator', $_SESSION['user_id']);
+            $this->db->bind(':id', $data['id']);
+            $this->db->execute();
+
+            $this->db->query('DELETE FROM transfers_details WHERE (header_id = :header_id)');
+            $this->db->bind(':header_id',$data['id']);
+            $this->db->execute();
+
+            $this->db->query('DELETE FROM stock_movements WHERE (transaction_type = :type) AND (transaction_id = :id)');
+            $this->db->bind(':type','transfer');
+            $this->db->bind(':id',$data['id']);
+            $this->db->execute();
+
+            foreach($data['items'] as $item){
+                $this->db->query('INSERT INTO transfers_details (header_id, product_id, qty) VALUES (:id, :product_id, :qty)');
+                $this->db->bind(':id', $data['id']);
+                $this->db->bind(':product_id', $item['product_id']);
+                $this->db->bind(':qty', $item['qty']);
+                $this->db->execute();
+
+                $this->db->query('INSERT INTO stock_movements (transaction_date,transaction_type,store_id,product_id,qty,transaction_id,created_by) VALUES (:date,:type,:store,:product,:qty,:tid,:creator)');
+                $this->db->bind(':date', $data['transfer_date']);
+                $this->db->bind(':type', 'transfer');
+                $this->db->bind(':store', $_SESSION['store']);
+                $this->db->bind(':product', $item['product_id']);
+                $this->db->bind(':qty', $item['qty'] * -1);
+                $this->db->bind(':tid', $data['id']);
+                $this->db->bind(':creator', $_SESSION['user_id']);
+                $this->db->execute();       
+            }
 
             if(!$this->db->dbh->commit()){
                 return false;
@@ -86,6 +145,29 @@ class Transfer
     {
         if(!$data['is_edit']){
             return $this->save($data);
+        }else{
+            return $this->update($data);
         }
+    }
+
+    public function get_transfer($id)
+    {
+        $count = (int)getdbvalue($this->db->dbh,'SELECT COUNT(*) FROM transfers_headers WHERE id = ?',[$id]);
+        if($count === 0) return false;
+
+        return singleset($this->db->dbh,'SELECT * FROM transfers_headers WHERE id = ?',[$id]);
+    }
+
+    public function get_transfer_items($id)
+    {
+        $sql = 'SELECT 
+                    t.product_id,
+                    p.product_name,
+                    t.qty 
+                FROM 
+                    transfers_details t 
+                    join products p on t.product_id = p.id 
+                WHERE t.header_id = ?';
+        return resultset($this->db->dbh,$sql,[$id]);
     }
 }
