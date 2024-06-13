7245ddc727554dfd57784b41da63c5c54241c825
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-13 21:45:20 +0300

    feat: creation of a receipt

--- app/controllers/Receipts.php
+++ app/controllers/Receipts.php
@@ -27,18 +27,163 @@ class Receipts extends Controller
     {
         $data = [
             'title' => 'Add receipt',
-            'stores' => $this->reusablemodel->get_stores(),
+            'stores' => $this->receiptmodel->get_transfering_stores(),
+            'transfers' => [],
             'products' => $this->reusablemodel->get_products_by_store($_SESSION['store']),
             'receipt_no' => $this->receiptmodel->get_receipt_no(),
             'is_edit' => false,
             'id' => '',
             'items' => [],
             'receipt_date' => date('Y-m-d'),
-            'store' => '',
-            'store_err' => '',
+            'store_from' => '',
+            'transfer_no' => '',
+            'store_from_err' => '',
             'receipt_date_err' => '',
+            'transfer_no_err' => '',
             'errors' => []
         ];
         $this->view('receipts/new',$data);
+    }
+
+    public function get_transfers()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'GET'){
+            http_response_code(405);
+            echo json_encode(['message' => 'Invalid request method.']);
+            exit();
+        }
+
+        $store = filter_input(INPUT_GET,'store',FILTER_SANITIZE_SPECIAL_CHARS);
+        if(empty($store)){
+            http_response_code(422);
+            echo json_encode(['message' => 'Store not provided.']);
+            exit();
+        }
+
+        $data = [];
+        foreach($this->receiptmodel->get_transfers($store) as $transfer){
+            array_push($data,[
+                'id' => $transfer->id,
+                'transfer_no' => $transfer->transfer_no
+            ]);
+        }
+
+        echo json_encode(['success' => true, 'message' => null, 'data' => $data]);
+    }
+
+    public function get_items()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'GET'){
+            http_response_code(405);
+            echo json_encode(['message' => 'Invalid request method.']);
+            exit();
+        }
+
+        $transfer_no = filter_input(INPUT_GET,'transferNo',FILTER_SANITIZE_SPECIAL_CHARS);
+        if(empty($transfer_no)){
+            http_response_code(422);
+            echo json_encode(['message' => 'Store not provided.']);
+            exit();
+        }
+
+        $data = [];
+        foreach($this->receiptmodel->get_items($transfer_no) as $item){
+            array_push($data,[
+                'id' => $item->product_id,
+                'product_name' => strtoupper($item->product_name),
+                'qty' => $item->qty
+            ]);
+        }
+
+        echo json_encode(['success' => true, 'message' => null, 'data' => $data]);
+    }
+
+    public function create_update()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+            flash('receipt_msg','Invalid request method.',alert_type('error'));
+            redirect('receipts/new');
+            exit();
+        }
+
+        $receipt_no = filter_input(INPUT_POST,'receipt_no',FILTER_SANITIZE_NUMBER_INT);
+        $receipt_date = filter_input(INPUT_POST,'receipt_date',FILTER_SANITIZE_SPECIAL_CHARS);
+        $store_from = filter_input(INPUT_POST,'store_from',FILTER_SANITIZE_SPECIAL_CHARS);
+        $transfer_no = filter_input(INPUT_POST,'transfer_no',FILTER_SANITIZE_SPECIAL_CHARS);
+        $product_ids = isset($_POST['product_id']) ? $_POST['product_id'] : [];
+        $products = isset($_POST['product']) ? $_POST['product'] : [];
+        $transfered_qtys = isset($_POST['transfered_qty']) ? $_POST['transfered_qty'] : [];
+        $received_qtys = isset($_POST['received_qty']) ? $_POST['received_qty'] : [];
+        $id = filter_input(INPUT_POST,'id',FILTER_SANITIZE_SPECIAL_CHARS);
+        $is_edit = filter_input(INPUT_POST,'is_edit',FILTER_VALIDATE_BOOLEAN);
+
+        $data = [
+            'title' => $is_edit ? 'Update receipt' : 'Add receipt',
+            'stores' => $this->receiptmodel->get_transfering_stores(),
+            'transfers' => [],
+            'products' => $this->reusablemodel->get_products_by_store($_SESSION['store']),
+            'receipt_no' => $is_edit ? $receipt_no : $this->receiptmodel->get_receipt_no(),
+            'is_edit' => $is_edit,
+            'id' => $is_edit ? $id : cuid(),
+            'items' => [],
+            'receipt_date' => !empty($receipt_date) ? date('Y-m-d',strtotime($receipt_date)) : '',
+            'store_from' => !empty($store_from) ? trim($store_from) : '',
+            'transfer_no' => !empty($transfer_no) ? $transfer_no : '',
+            'store_from_err' => '',
+            'receipt_date_err' => '',
+            'transfer_no_err' => '',
+            'errors' => []
+        ];
+
+        if(empty($data['receipt_date'])){
+            $data['receipt_date_err'] = 'Select receipt date';
+        }
+        if(empty($data['store_from'])){
+            $data['store_from_err'] = 'Select store from';
+        }else{
+            $data['transfers'] = $this->receiptmodel->get_transfers($data['store_from']);
+        }
+        if(empty($data['transfer_no'])){
+            $data['transfer_no_err'] = 'Select transfer no';
+        }else{
+            for ($i=0; $i < count($products); $i++) { 
+                array_push($data['items'],[
+                    'product_id' => $product_ids[$i],
+                    'product_name' => $products[$i],
+                    'transfered_qty' => $transfered_qtys[$i],
+                    'received_qty' => $received_qtys[$i],
+                ]);
+            }
+        }
+
+        if(!empty($data['receipt_date_err']) || !empty($data['transfer_no_err'] || !empty($data['store_from_err']))){
+            $this->view('receipts/new',$data);
+            exit();
+        }
+        if($this->receiptmodel->date_is_earlier($data['transfer_no'],$data['receipt_date'])){
+            array_push($data['errors'],'Receipt date cannot be earlier than transfer date.');
+            $this->view('receipts/new',$data);
+            exit();
+        }
+        foreach($data['items'] as $item){
+            if(empty($item['received_qty'])){
+                array_push($data['errors'],'Received qty not provided for '.ucwords($item['product_name']));
+            }
+            if((int)$item['transfered_qty'] < (int)$item['received_qty']){
+                array_push($data['errors'],'Received more than transfered for '.ucwords($item['product_name']));
+            }
+        }
+        if(count($data['errors']) > 0){
+            $this->view('receipts/new',$data);
+            exit();
+        }
+
+        if(!$this->receiptmodel->create_update($data)){
+            array_push($data['errors'],'Failed to save transfer.');
+            $this->view('receipts/new',$data);
+            exit();
+        }
+
+        redirect('receipts');
     }
 }
--- app/models/Receipt.php
+++ app/models/Receipt.php
@@ -3,4 +3,126 @@
 class Receipt
 {
     private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function get_receipt_no()
+    {
+        return get_next_db_no($this->db->dbh,'receipts_headers','receipt_no');
+    }
+
+    public function get_transfering_stores()
+    {
+        $sql = "SELECT
+                    t.store_from as id,
+                    s.store_name
+                FROM
+                    transfers_headers t join stores s on t.store_from = s.ID
+                WHERE
+                    (t.id NOT IN (SELECT transfer_id FROM receipts_headers))
+                GROUP BY
+                    t.store_from,
+                    s.store_name";
+
+        return resultset($this->db->dbh,$sql,[]);
+    }
+
+    public function get_transfers($store)
+    {
+        $sql = "SELECT
+                    id,
+                    transfer_no
+                FROM
+                    transfers_headers 
+                WHERE
+                    (store_from = ?) AND (id NOT IN (SELECT transfer_id FROM receipts_headers))
+                ORDER BY
+                    transfer_no DESC
+                ";
+
+        return resultset($this->db->dbh,$sql,[$store]);
+    }
+
+    public function get_items($transfer_no)
+    {
+        $sql = "SELECT
+                    product_id,
+                    product_name,
+                    qty
+                FROM
+                    transfers_details t join products p on t.product_id = p.id
+                WHERE
+                    (header_id = ?)
+                ";
+
+        return resultset($this->db->dbh,$sql,[$transfer_no]);
+    }
+
+    public function date_is_earlier($transfer,$date)
+    {
+        $transfer_date = getdbvalue($this->db->dbh,'SELECT transfer_date FROM transfers_headers WHERE id=?',[$transfer]);
+        return $date < date('Y-m-d',strtotime($transfer_date));
+    }
+
+    function save($data)
+    {
+        try {
+            $receipt_no = $this->get_receipt_no();
+            $this->db->dbh->beginTransaction();
+
+            $sql = 'INSERT INTO receipts_headers (id,receipt_date, receipt_no, transfer_id, created_by) 
+                    VALUES(:id,:receipt_date,:receipt_no,:transfer_id,:creator)';
+            $this->db->query($sql);
+            $this->db->bind(':id', $data['id']);
+            $this->db->bind(':receipt_date',$data['receipt_date']);
+            $this->db->bind(':receipt_no', $receipt_no);
+            $this->db->bind(':transfer_id', $data['transfer_no']);
+            $this->db->bind(':creator', $_SESSION['user_id']);
+            $this->db->execute();
+
+            foreach($data['items'] as $item){
+                $sql = 'INSERT INTO receipts_details (header_id, product_id, transfered_qty, received_qty)
+                        VALUES (:id, :product_id, :transfered_qty, :received_qty)';
+                $this->db->query($sql);
+                $this->db->bind(':id', $data['id']);
+                $this->db->bind(':product_id', $item['product_id']);
+                $this->db->bind(':transfered_qty', $item['transfered_qty']);
+                $this->db->bind(':received_qty', $item['received_qty']);
+                $this->db->execute();
+
+                $this->db->query('INSERT INTO stock_movements (transaction_date,transaction_type,store_id,product_id,qty,transaction_id,created_by) VALUES (:date,:type,:store,:product,:qty,:tid,:creator)');
+                $this->db->bind(':date', $data['receipt_date']);
+                $this->db->bind(':type', 'receipt');
+                $this->db->bind(':store', $_SESSION['store']);
+                $this->db->bind(':product', $item['product_id']);
+                $this->db->bind(':qty', $item['received_qty']);
+                $this->db->bind(':tid', $data['id']);
+                $this->db->bind(':creator', $_SESSION['user_id']);
+                $this->db->execute();        
+            }
+
+            if(!$this->db->dbh->commit()){
+                return false;
+            }
+            
+            return true;
+            
+        } catch (\Exception $e) {
+            if($this->db->dbh->inTransaction()){
+                $this->db->dbh->rollBack();
+            }
+            error_log($e->getMessage(),0);
+            return false;
+        }
+    }
+
+    public function create_update($data)
+    {
+        if(!$data['is_edit']){
+            return $this->save($data);
+        }
+    }
 }
