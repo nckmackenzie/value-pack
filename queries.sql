49fcbf08d757b7057c952926ad465d846c0aa0e6
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-13 21:09:27 +0300

    chore: creation of receipts table

--- queries.sql
+++ queries.sql
@@ -1,58 +1,43 @@
 START TRANSACTION;
 
-CREATE TABLE `transfers_headers` (
+CREATE TABLE `receipts_headers` (
   `id` VARCHAR(100) NOT NULL, 
-  `transfer_date` DATE NOT NULL, 
-  `store_from` VARCHAR(100) NOT NULL, 
-  `store_to` VARCHAR(100) NOT NULL, 
-  `transfer_no` INT NOT NULL, 
+  `receipt_date` DATE NOT NULL, 
+  `receipt_no` INT NOT NULL, 
+  `transfer_id` VARCHAR(100) NOT NULL, 
   `created_by` VARCHAR(100) NOT NULL, 
-  `created_on` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-  PRIMARY KEY (
-    `id`(100)
-  )
-) ENGINE = InnoDB;
-
-ALTER TABLE 
-  `transfers_headers` 
-ADD 
-  CONSTRAINT `fk_transfer_store_from` FOREIGN KEY (`store_from`) REFERENCES `stores`(`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE 
-  `transfers_headers` 
-ADD 
-  CONSTRAINT `fk_transfer_store_to` FOREIGN KEY (`store_to`) REFERENCES `stores`(`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE 
-  `transfers_headers` 
-ADD 
-  CONSTRAINT `fk_transfer_user` FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
-CREATE TABLE `transfers_details` (
-  `id` INT NOT NULL AUTO_INCREMENT, 
-  `header_id` VARCHAR(100) NOT NULL, 
-  `product_id` VARCHAR(100) NOT NULL, 
-  `qty` DECIMAL(18, 2) NOT NULL, 
+  `created_on` INT NOT NULL DEFAULT CURRENT_TIMESTAMP, 
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
 
 ALTER TABLE 
-  `transfers_details` 
+  `receipts_headers` 
 ADD 
-  CONSTRAINT `fk_transfer_details_header_id` FOREIGN KEY (`header_id`) REFERENCES `transfers_headers`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  CONSTRAINT `fk_receipt_transfer_id` FOREIGN KEY (`transfer_id`) REFERENCES `transfers_headers`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 ALTER TABLE 
-  `transfers_details` 
+  `receipts_headers` 
 ADD 
-  CONSTRAINT `fk_transfer_details_product_id` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  CONSTRAINT `fk_receipt_user_id` FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-INSERT INTO `forms` (
-  `id`, `form_name`, `module`, `module_id`, 
-  `path`, `menu_order`
-) 
-VALUES 
-  (
-    NULL, 'receipts', 'transactions', 
-    '15', 'receipts', '15'
-  );
+
+CREATE TABLE `receipts_details` (
+  `id` INT NOT NULL AUTO_INCREMENT, 
+  `header_id` VARCHAR(100) NOT NULL, 
+  `product_id` VARCHAR(100) NOT NULL, 
+  `transfered_qty` DECIMAL(18, 2) NOT NULL, 
+  `received_qty` DECIMAL(18, 2) NOT NULL, 
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+ALTER TABLE 
+  `receipts_details` 
+ADD 
+  CONSTRAINT `fk_receipt_details_header_id` FOREIGN KEY (`header_id`) REFERENCES `receipts_headers`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE 
+  `receipts_details` 
+ADD 
+  CONSTRAINT `fk_receipt_details_product_id` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 
 COMMIT;
