d158478afc5d161e7abad831e96729809388f18f
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-16 20:47:49 +0300

    feat: functionality to create a user

---
+++ app/controllers/Users.php
@@ -0,0 +1,132 @@
+<?php
+class Users extends Controller
+{
+    private $authmodel;
+    private $usermodel;
+    private $reusablemodel;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->authmodel = $this->model('Auths');
+        $this->usermodel = $this->model('User');
+        $this->reusablemodel = $this->model('Reusable');
+        check_rights($this->authmodel,'users');
+    }
+
+    public function index()
+    {
+        $data = [
+            'title' => 'Users',
+            'users' => $this->usermodel->get_users()
+        ];
+        $this->view('users/index', $data);
+    }
+
+    public function new()
+    {
+        $data = [
+            'title' => 'Create user',
+            'stores' => $this->reusablemodel->get_stores(),
+            'roles' => $this->usermodel->get_roles(),
+            'id' => '',
+            'is_edit' => false,
+            'user_name' => '',
+            'password' => '',
+            'confirm_password' => '',
+            'contact' => '',
+            'role' => '',
+            'active' => true,
+            'stores_allowed' => [],
+            'user_name_err' => '',
+            'password_err' => '',
+            'confirm_password_err' => '',
+            'role_err' => '',
+            'contact_err' => '',
+            'store_err' => '',
+            'error' => null
+        ];
+        $this->view('users/new', $data);
+    }
+
+    public function create_update()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+            flash('user_msg','Invalid request method.', alert_type('error'));
+            redirect('users/new');
+            exit();
+        }
+
+        $user_name = filter_input(INPUT_POST,'user_name', FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST,'password', FILTER_SANITIZE_SPECIAL_CHARS);
+        $contact = filter_input(INPUT_POST,'contact', FILTER_SANITIZE_SPECIAL_CHARS);
+        $confirm_password = filter_input(INPUT_POST,'confirm_password', FILTER_SANITIZE_SPECIAL_CHARS);
+        $role = filter_input(INPUT_POST,'role', FILTER_SANITIZE_NUMBER_INT);
+        $id = filter_input(INPUT_POST,'id', FILTER_SANITIZE_SPECIAL_CHARS);
+        $is_edit = filter_input(INPUT_POST,'is_edit',FILTER_VALIDATE_BOOLEAN);
+
+        $data = [
+            'title' => $is_edit ? 'Update user' : 'Create user',
+            'stores' => $this->reusablemodel->get_stores(),
+            'roles' => $this->usermodel->get_roles(),
+            'id' => $is_edit ? $id : cuid(true),
+            'is_edit' => $is_edit,
+            'user_name' => !empty($user_name) ? trim($user_name) : '',
+            'password' => !empty($password) ? trim($password) : '',
+            'confirm_password' => !empty($confirm_password) ? trim($confirm_password) : '',
+            'contact' => !empty($contact) ? trim($contact) : '',
+            'role' => !empty($role) ? (int)$role : '',
+            'stores_allowed' => isset($_POST['store'])  ? $_POST['store'] : [],
+            'active' => $is_edit ? filter_input(INPUT_POST,'active',FILTER_VALIDATE_BOOLEAN) : true,
+            'user_name_err' => '',
+            'password_err' => '',
+            'confirm_password_err' => '',
+            'role_err' => '',
+            'contact_err' => '',
+            'store_err' => '',
+            'error' => null
+        ];
+
+        if(empty($data['user_name'])){
+            $data['user_name_err'] = 'Enter user name';
+        }
+        if(empty($data['password'])){
+            $data['password_err'] = 'Enter password';
+        }
+        if(empty($data['confirm_password'])){
+            $data['confirm_password_err'] = 'Confirm password';
+        }
+        if(empty($data['contact'])){
+            $data['contact_err'] = 'Enter phone number';
+        }
+        if(empty($data['role'])){
+            $data['role_err'] = 'Select user role';
+        }
+        if(count($data['stores_allowed']) === 0){
+            $data['store_err'] = 'Select at least one store';
+        }
+
+        if(!empty($data['password']) && !empty($data['confirm_password']) && ($data['password'] !== $data['confirm_password'])){
+            $data['confirm_password_err'] = 'Password not matched';
+        }
+
+        if(!empty($data['user_name_err']) || !empty($data['password_err']) || !empty($data['confirm_password_err']) 
+            || !empty($data['role_err']) || !empty($data['contact_err']) || !empty($data['store_err'])){
+                $this->view('users/new', $data);
+                exit();
+        }
+
+        if($this->usermodel->contact_exists($data['contact'],$data['id'])){
+            $data['error'] = 'Contact already exists.';
+            $this->view('users/new', $data);
+            exit();
+        }
+
+        if(!$this->usermodel->create_update($data)){
+            $data['error'] = 'Something went wrong. Please try again later.';
+            $this->view('users/new', $data);
+            exit();
+        }
+
+        redirect('users');
+    }
+}
---
+++ app/models/User.php
@@ -0,0 +1,80 @@
+<?php
+class User
+{
+    private $db;
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+    public function get_users()
+    {
+        $sql = 'SELECT u.id, user_name, contact, role_name, active
+                FROM users u join roles r on u.role_id = r.id';
+        return resultset($this->db->dbh,$sql,[]);
+    }
+
+    public function get_roles()
+    {
+        return resultset($this->db->dbh,'SELECT * from roles ORDER BY role_name',[]);
+    }
+
+    public function contact_exists($contact,$id)
+    {
+        $sql = 'SELECT count(*) FROM users WHERE contact = ? AND id <> ?';
+        return (int)getdbvalue($this->db->dbh,$sql,[$contact,$id]) > 0;
+    }
+
+    public function create_update($data)
+    {
+        try {
+            
+            $this->db->dbh->beginTransaction();
+
+            if(!$data['is_edit']){
+                $sql = "INSERT INTO users (id, user_name, password, contact, role_id, active) 
+                        VALUES(:id, :user_name, :password, :contact, :role_id, :active)";
+                $this->db->query($sql);
+                $this->db->bind(':id', $data['id']);
+                $this->db->bind(':user_name', strtolower($data['user_name']));
+                $this->db->bind(':password', password_hash($data['password'], PASSWORD_DEFAULT));
+                $this->db->bind(':contact', $data['contact']);
+                $this->db->bind(':role_id', $data['role']);
+                $this->db->bind(':active', $data['active']);
+                $this->db->execute();
+            }else{
+                $sql = "UPDATE users SET user_name = :user_name, contact = :contact, role_id = :role_id, active = :active 
+                        WHERE (id = :id)";
+                $this->db->query($sql);
+                $this->db->bind(':user_name', strtolower($data['user_name']));
+                $this->db->bind(':contact', $data['contact']);
+                $this->db->bind(':role_id', $data['role']);
+                $this->db->bind(':active', $data['active']);
+                $this->db->bind(':id', $data['id']);
+                $this->db->execute();
+
+                $this->db->query('DELETE FROM user_stores WHERE user_id = :id');
+                $this->db->bind(':id', $data['id']);
+                $this->db->execute();
+            }
+
+            foreach($data['stores_allowed'] as $store){
+                $this->db->query('INSERT INTO user_stores (user_id, store_id) VALUES(:user_id, :store_id)');
+                $this->db->bind(':user_id', $data['id']);
+                $this->db->bind(':store_id', $store);
+                $this->db->execute();
+            }
+
+            if(!$this->db->dbh->commit()){
+                return false;
+            }
+            return true;
+
+        } catch (\Exception $e) {
+            if($this->db->dbh->inTransaction()){
+                $this->db->dbh->rollBack();
+            }
+            error_log($e->getMessage());
+            return false;
+        }
+    }
+}
