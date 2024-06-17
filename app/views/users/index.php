da7c6e478f6043aae50f3b992ea6de5457e62afc
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-17 16:10:26 +0300

    refactor: add action to reset user password

--- app/views/users/index.php
+++ app/views/users/index.php
@@ -48,7 +48,7 @@
               <td class="flex items-center gap-2">
                 <?php action_buttons("edit","users",$user->id);?>
                 <?php if((bool)$user->active) : ?>
-                  <a href="<?php echo URLROOT;?>/users/reset-password/<?php echo $user->id;?>" class='group'>
+                  <a href="<?php echo URLROOT;?>/users/reset_password/<?php echo $user->id;?>" class='group'>
                       <span class='text-sky-400 transition-colors font-medium text-xs group-hover:text-sky-300'>Reset Password</span>
                   </a>
                 <?php endif; ?>
