5489c888a8f7a621a664e2d5298580fa0cbd6801
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-17 06:02:38 +0300

    feat:  users datatable and multiselect

--- public/js/pages/users/user.js
+++ public/js/pages/users/user.js
@@ -3,3 +3,15 @@ basicDatatable('usersDatatable', [
   { width: '10%', targets: 3 },
   { width: '15%', targets: 4 },
 ]);
+
+function activateMultiSelect() {
+  $(function () {
+    $('#store').multiselect({
+      includeSelectAllOption: true,
+      buttonWidth: '100%',
+      maxHeight: 200,
+    });
+  });
+}
+
+activateMultiSelect();
