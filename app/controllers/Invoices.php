f1193d47e5e9346e907fbab602ec48fe6f16ad5f
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-19 08:00:15 +0300

    feat: functionality to create invoice

---
+++ app/controllers/Invoices.php
@@ -0,0 +1,144 @@
+<?php
+class Invoices extends Controller
+{
+    private $authmodel;
+    private $invoicemodel;
+    private $reusablemodel;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->authmodel = $this->model('Auths');
+        $this->invoicemodel = $this->model('Invoice');
+        $this->reusablemodel = $this->model('Reusable');
+        check_rights($this->authmodel,'invoices');
+    }
+
+    public function index()
+    {
+        $data = [
+            'title' => 'Invoices',
+            'invoices' => []
+        ];
+        $this->view('invoices/index', $data);
+    }
+
+    public function new()
+    {
+        $data = [
+            'title' => 'Create invoice',
+            'customers' => $this->reusablemodel->get_customers(),
+            'products' => $this->reusablemodel->get_products_by_store($_SESSION['store']),
+            'invoice_no' => $this->invoicemodel->get_invoice_no(),
+            'id' => '',
+            'is_edit' => false,
+            'date' => date('Y-m-d'),
+            'customer' => '',
+            'vat_type' => '',
+            'vat' => '',
+            'items' => [],
+            'total' => 0,
+            'date_err' => '',
+            'customer_err' => '',
+            'vat_type_err' => '',
+            'vat_err' => '',
+            'errors' => []
+           ];
+        $this->view('invoices/new', $data);
+    }
+
+    public function create_update()
+    {
+        if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+            flash('invoice_msg','Invalid request method.');
+            redirect('invoices');
+            exit();
+        }
+
+        $date = filter_input(INPUT_POST, 'date', FILTER_SANITIZE_SPECIAL_CHARS);
+        $customer = filter_input(INPUT_POST, 'customer', FILTER_SANITIZE_SPECIAL_CHARS);
+        $vat_type = filter_input(INPUT_POST, 'vat_type', FILTER_SANITIZE_SPECIAL_CHARS);
+        $vat = filter_input(INPUT_POST, 'vat', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
+        $is_edit = filter_input(INPUT_POST,'is_edit',FILTER_VALIDATE_BOOLEAN);
+        $products = isset($_POST['product']) ? $_POST['product'] : [];
+        $product_ids = isset($_POST['product_id']) ? $_POST['product_id'] : [];
+        $quantities = isset($_POST['qty']) ? $_POST['qty'] : [];
+        $rates = isset($_POST['rate']) ? $_POST['rate'] : [];
+        
+        $data = [
+            'title' => $is_edit ? 'Update invoice' : 'Create invoice',
+            'products' => $this->reusablemodel->get_products_by_store($_SESSION['store']),
+            'customers' => $this->reusablemodel->get_customers(),
+            'stores' => $this->reusablemodel->get_stores(),
+            'id' => $is_edit ? $id : cuid(),
+            'is_edit' => $is_edit,
+            'date' => !empty($date) ? date('Y-m-d',strtotime($date)) : null ,
+            'customer' => !empty($customer) ? trim($customer) : null,
+            'invoice_no' => !$is_edit ? $this->invoicemodel->get_invoice_no() : null,
+            'vat_type' => !empty($vat_type) ? trim($vat_type) : null,
+            'vat' => !empty($vat) ? floatval($vat) : null,
+            'items' => [],
+            'total' => 0,
+            'customer_err' => '',
+            'date_err' => '',
+            'vat_type_err' => '',
+            'vat_err' => '',
+            'errors' => []
+        ];
+
+        if(is_null($data['date'])){
+            $data['date_err'] = 'Select date';
+        }
+        if(is_null($data['customer'])){
+            $data['customer_err'] = 'Select customer';
+        }
+        if(is_null($data['vat_type'])){
+            $data['vat_type_err'] = 'Select vat type';
+        }
+        if(!is_null($data['vat_type']) && $data['vat_type'] !== 'no-vat' && is_null($data['vat'])){
+            $data['vat_err'] = 'Select vat';
+        }
+
+        if(count($products) === 0){
+            array_push($data['errors'], 'No products added.');
+        }else{
+            for ($i=0; $i < count($products); $i++) { 
+                array_push($data['items'],[
+                    'product_id' => $product_ids[$i],
+                    'product_name' => $products[$i],
+                    'qty' => $quantities[$i],
+                    'rate' => $rates[$i],
+                    'value' => floatval($quantities[$i]) * floatval($rates[$i])
+                ]);
+            }
+        }
+
+        if(count($data['errors']) > 0 || !empty($data['date_err']) || !empty($data['store_err']) || !empty($data['vat_type_err'])
+            || !empty($data['vat_err']) || !empty($data['store_err'])){
+            $this->view('invoices/new', $data);
+            exit();
+        }
+
+        $data['total'] = array_sum(array_column($data['items'], 'value'));
+
+        foreach($data['items'] as $item){
+            $curr_value = $this->reusablemodel->get_current_stock_balance($_SESSION['store'],$item['product_id'],$data['date']);
+            if((int)$curr_value < (int)$item['qty']){
+                array_push($data['errors'],'Insufficient stock for product '.$item['product_name']);
+            }
+        }
+
+        if(count($data['errors']) > 0){
+            $this->view('invoices/new',$data);
+            exit();
+        }
+
+        if(!$this->invoicemodel->create_update($data)){
+            array_push($data['errors'], 'Something went wrong while performing this action.');
+            $this->view('invoices/new',$data);
+            exit();
+        }
+
+        redirect('invoices');
+    }
+}
---
+++ app/models/Invoice.php
@@ -0,0 +1,149 @@
+<?php
+
+class invoice
+{
+    private $db;
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function get_invoice_no()
+    {
+        return get_next_db_no($this->db->dbh,'invoices_headers','invoice_no');
+    }
+
+    function save($data)
+    {
+        try {
+
+            $this->db->dbh->beginTransaction();
+
+            $sql = "INSERT INTO invoices_headers (id, invoice_date, customer_id, invoice_no, vat_type, vat, exclusive_amount, vat_amount, inclusive_amount, store_id, created_by) ";
+            $sql .= "VALUES (:id, :date, :customer, :invoice_no, :vat_type, :vat, :amount_exclusive, :vat_amount, :amount_inclusive, :store, :creator) ";
+
+            $this->db->query($sql);
+            $this->db->bind(':id', $data['id']);
+            $this->db->bind(':date', $data['date']);
+            $this->db->bind(':customer', $data['customer']);
+            $this->db->bind(':invoice_no', strtolower($data['invoice_no']));
+            $this->db->bind(':vat_type', $data['vat_type']);
+            $this->db->bind(':vat', $data['vat']);            
+            $this->db->bind(':amount_exclusive', calculate_vat($data['vat_type'],$data['total'])[0]);
+            $this->db->bind(':vat_amount', calculate_vat($data['vat_type'],$data['total'])[1]);
+            $this->db->bind(':amount_inclusive', calculate_vat($data['vat_type'],$data['total'])[2]);
+            $this->db->bind(':store', $_SESSION['store']);
+            $this->db->bind(':creator', $_SESSION['user_id']);
+            $this->db->execute();
+
+            foreach($data['items'] as $item){
+                $this->db->query('INSERT INTO invoices_details (header_id, product_id, qty, rate, gross) VALUES (:id, :product_id, :qty, :rate, :gross)');
+                $this->db->bind(':id', $data['id']);
+                $this->db->bind(':product_id', $item['product_id']);
+                $this->db->bind(':qty', $item['qty']);
+                $this->db->bind(':rate', $item['rate']);
+                $this->db->bind(':gross', floatval($item['qty']) * floatval($item['rate']));
+                $this->db->execute();
+
+                $this->db->query('INSERT INTO stock_movements (transaction_date,transaction_type,store_id,product_id,qty,transaction_id,created_by) VALUES (:date,:type,:store,:product,:qty,:tid,:creator)');
+                $this->db->bind(':date', $data['date']);
+                $this->db->bind(':type', 'invoice');
+                $this->db->bind(':store', $_SESSION['store']);
+                $this->db->bind(':product', $item['product_id']);
+                $this->db->bind(':qty', $item['qty'] * -1);
+                $this->db->bind(':tid', $data['id']);
+                $this->db->bind(':creator', $_SESSION['user_id']);
+                $this->db->execute();
+            }
+
+            if(!$this->db->dbh->commit()){
+                return false;
+            }
+
+            return true;
+            
+        } catch (\Exception $e) {
+            if ($this->db->dbh->inTransaction()) {
+                $this->db->dbh->rollback();
+            }
+            error_log($e->getMessage());
+            return false;
+        }
+    }
+
+    function update($data)
+    {
+        try {
+
+            $this->db->dbh->beginTransaction();
+
+            $sql = "UPDATE invoices_headers SET invoice_date = :date, customer_id = :customer, vat_type = :vat_type, vat = :vat, 
+                                               exclusive_amount = :amount_exclusive, vat_amount = :vat_amount, inclusive_amount = :amount_inclusive, created_by = :creator 
+                    WHERE id = :id";
+            
+            $this->db->query($sql);
+            $this->db->bind(':date', $data['date']);
+            $this->db->bind(':customer', $data['customer']);
+            $this->db->bind(':vat_type', $data['vat_type']);
+            $this->db->bind(':vat', $data['vat']);            
+            $this->db->bind(':amount_exclusive', calculate_vat($data['vat_type'],$data['total'])[0]);
+            $this->db->bind(':vat_amount', calculate_vat($data['vat_type'],$data['total'])[1]);
+            $this->db->bind(':amount_inclusive', calculate_vat($data['vat_type'],$data['total'])[2]);
+            $this->db->bind(':creator', $_SESSION['user_id']);
+            $this->db->bind(':id', $data['id']);
+            $this->db->execute();
+
+            $this->db->query('DELETE FROM invoice_details WHERE (header_id = :header_id)');
+            $this->db->bind(':header_id',$data['id']);
+            $this->db->execute();
+
+            $this->db->query('DELETE FROM stock_movements WHERE (transaction_type = :type) AND (transaction_id = :id)');
+            $this->db->bind(':type','invoice');
+            $this->db->bind(':id',$data['id']);
+            $this->db->execute();
+
+            foreach($data['items'] as $item){
+                $this->db->query('INSERT INTO invoices_details (header_id, product_id, qty, rate, gross) VALUES (:id, :product_id, :qty, :rate, :gross)');
+                $this->db->bind(':id', $data['id']);
+                $this->db->bind(':product_id', $item['product_id']);
+                $this->db->bind(':qty', $item['qty']);
+                $this->db->bind(':rate', $item['rate']);
+                $this->db->bind(':gross', floatval($item['qty']) * floatval($item['rate']));
+                $this->db->execute();
+
+                $this->db->query('INSERT INTO stock_movements (transaction_date,transaction_type,store_id,product_id,qty,transaction_id,created_by) VALUES (:date,:type,:store,:product,:qty,:tid,:creator)');
+                $this->db->bind(':date', $data['date']);
+                $this->db->bind(':type', 'invoice');
+                $this->db->bind(':store', $_SESSION['store']);
+                $this->db->bind(':product', $item['product_id']);
+                $this->db->bind(':qty', $item['qty'] * -1);
+                $this->db->bind(':tid', $data['id']);
+                $this->db->bind(':creator', $_SESSION['user_id']);
+                $this->db->execute();
+            }
+
+            if(!$this->db->dbh->commit()){
+                return false;
+            }
+
+            return true;
+            
+        } catch (\Exception $e) {
+            if ($this->db->dbh->inTransaction()) {
+                $this->db->dbh->rollback();
+            }
+            error_log($e->getMessage());
+            return false;
+        }
+    }
+
+    public function create_update($data)
+    {
+        if(!$data['is_edit']){
+            return $this->save($data);
+        }else{
+            return $this->update($data);
+        }
+    }
+
+}
