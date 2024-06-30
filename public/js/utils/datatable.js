cce74a6970f87c4c84336e396aeed4cb7effdb70
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-30 14:38:00 +0300

    feat: add datatable with buttons

--- public/js/utils/datatable.js
+++ public/js/utils/datatable.js
@@ -19,3 +19,32 @@ export function deleteButtonClick(datatableId, modalId, inputId) {
     });
   });
 }
+
+export const reportDatatable = table => {
+  new DataTable(`#${table}`, {
+    layout: {
+      topStart: {
+        buttons: [
+          {
+            extend: 'copyHtml5',
+            exportOptions: {
+              columns: [0, ':visible'],
+            },
+          },
+          {
+            extend: 'excelHtml5',
+            exportOptions: {
+              columns: ':visible',
+            },
+          },
+          {
+            extend: 'pdfHtml5',
+            exportOptions: {
+              columns: [0, 1, 2, 5],
+            },
+          },
+        ],
+      },
+    },
+  });
+};
