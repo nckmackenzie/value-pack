ab88bf5f4c0e77e946c5d3eb79de85e8a78e769a
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-30 10:30:02 +0300

    chore: fallback pages for reports

---
+++ app/views/reports/index.php
@@ -0,0 +1,18 @@
+<?php require APPROOT . '/views/inc/header.php';?>
+<?php require APPROOT . '/views/inc/top-nav.php';?>
+<?php require APPROOT . '/views/inc/side-nav.php';?>
+  <div class="content-wrapper px-4">
+    <section class="content-header px-0">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          
+        </div>
+      </div>
+    </section>
+    <section class="content">
+
+    </section><!-- /.content -->
+</div><!-- /.content-wrapper -->
+<?php require APPROOT . '/views/inc/footer.php'?>
+</body>
+</html>  
