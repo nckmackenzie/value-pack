ea30dc02cc265cbcb99b99db1c7ba439c56b2803
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-08-05 10:08:58 +0300

    chore: add util function to format numbers

--- public/js/utils/formatters.js
+++ public/js/utils/formatters.js
@@ -1,3 +1,9 @@
 export function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
+
+export function numberFormatter(number) {
+  return new Intl.NumberFormat('en-KE', { maximumFractionDigits: 2 }).format(
+    Number(number)
+  );
+}
